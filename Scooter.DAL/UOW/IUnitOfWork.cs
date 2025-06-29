c156ec33626dcc78cd592b3bbc2165f64a50c0ed
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-06-30 00:17:43 +0300

    add unit of work

---
+++ Scooter.DAL/UOW/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using ScooterDAL.Repositories.Interfaces;
+
+namespace ScooterDAL.UOW
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+        IVehicleRepository Vehicles { get; }
+        ITripRepository Trips { get; }
+        IPaymentRepository Payments { get; }
+        IFeedbackRepository Feedbacks { get; }
+
+        Task<int> SaveAsync();
+    }
+}
---
+++ Scooter.DAL/UOW/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using ScooterDAL.Data;
+using ScooterDAL.Repositories.Interfaces;
+
+namespace ScooterDAL.UOW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ScooterDbContext _context;
+
+        public IUserRepository Users { get; }
+        public IVehicleRepository Vehicles { get; }
+        public ITripRepository Trips { get; }
+        public IPaymentRepository Payments { get; }
+        public IFeedbackRepository Feedbacks { get; }
+
+        public UnitOfWork(ScooterDbContext context,
+                          IUserRepository users,
+                          IVehicleRepository vehicles,
+                          ITripRepository trips,
+                          IPaymentRepository payments,
+                          IFeedbackRepository feedbacks)
+        {
+            _context = context;
+            Users = users;
+            Vehicles = vehicles;
+            Trips = trips;
+            Payments = payments;
+            Feedbacks = feedbacks;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
