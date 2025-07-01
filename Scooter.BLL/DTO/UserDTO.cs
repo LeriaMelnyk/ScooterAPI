830ed84293069ff9d6f6f1fb45dde6667ac96fd6
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-07-01 21:33:09 +0300

    add DTOs with model state

---
+++ Scooter.BLL/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Scooter.BLL.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Повне ім'я є обов'язковим")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Ім'я повинно містити від 2 до 100 символів")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email є обов'язковим")]
+        [EmailAddress(ErrorMessage = "Невірний формат email")]
+        public string Email { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Scooter.BLL/DTO/VehicleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scooter.BLL.DTO
+{
+    public class VehicleDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Модель транспортного засобу є обов'язковою")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Модель повинна містити від 2 до 100 символів")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Реєстраційний номер є обов'язковим")]
+        [StringLength(20, ErrorMessage = "Реєстраційний номер не може бути довшим за 20 символів")]
+        public string RegistrationNumber { get; set; }
+
+        public bool IsAvailable { get; set; }
+    }
+}
